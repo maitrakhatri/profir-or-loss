ca7d6a90f5f11ce449e90fc70b75e79aa6aa85d5
maitrakhatri <maitrakhatri@gmail.com>
2021-09-24 23:54:00 +0530

    logic done

---
+++ app.js
@@ -0,0 +1,57 @@
+var buyPrice = document.querySelector("#initial-price");
+var stockQuant = document.querySelector("#no-of-stocks");
+var currentPrice = document.querySelector("#current-price");
+var calculateButton = document.querySelector("#calculate-button");
+var result = document.querySelector("#result");
+
+function showMessage(message) {
+    result.style.display = "block";
+    result.innerText = message;
+}
+
+function calculate() {
+
+    var ip = Number(buyPrice.value);
+    var sq = Number(stockQuant.value);
+    var cp = Number(currentPrice.value);
+
+    var netValue = cp - ip;
+
+    if(netValue > 0) {
+        
+        var netProfit = netValue*sq;
+        var profitPercentage = (netProfit/ip)*100;
+        showMessage("Your profit is: " + netProfit + " and Your profit perentage is: " + profitPercentage + "%");
+        
+    }
+
+    if(netValue == 0) {
+        showMessage("No profit No loss")
+        
+    }
+
+    if(netValue < 0) {
+        
+        var netLoss = Math.abs(netValue*sq);
+        var lossPercentage = (netLoss/ip)*100;
+        showMessage("Your loss is: " + netLoss + " and Your loss percentage is: " + lossPercentage + "%");
+
+    }
+
+}
+
+function clickHander(){
+
+    result.style.display = "none";
+
+    if(buyPrice.value == "" || stockQuant.value == "" || currentPrice.value == "" || buyPrice.value < 0 || stockQuant.value < 0 || currentPrice.value < 0) {
+
+        showMessage("Enter all the Positive Values")
+    }
+
+    else{
+        calculate();
+    }
+}
+
+calculateButton.addEventListener("click", clickHander)
